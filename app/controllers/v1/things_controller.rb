03f3292592643089cfeb1c948b990d636e57800d
Hamza <hamzatarar8@gmail.com>
2022-05-19 14:45:38 +0200

    Corrected linters

--- app/controllers/v1/things_controller.rb
+++ app/controllers/v1/things_controller.rb
@@ -1,9 +1,9 @@
 class V1::ThingsController < ApplicationController
   def index
-    render json: { :things => [
+    render json: { things: [
       {
-        :name => 'some-thing',
-        :guid => '01541-821-5-11'
+        name: 'some-thing',
+        guid: '01541-821-5-11'
       }
     ] }.to_json
   end
